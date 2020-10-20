2291f70a7b17540622e21f615b9f9283d540a58f
Brandon Leong <bjtl1105@gmail.com>
2020-10-20 10:55:18 -0700

    finished prelim dashboard styling

--- frontend/components/Nav_bar/nav_bar.jsx
+++ frontend/components/Nav_bar/nav_bar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
 import LoginModal from '../login_module/login_modal';
 import SignInModal from '../login_module/signup_modal';
 import DashboardContainer from '../user_page/dashboard_container';
@@ -11,7 +12,7 @@ class NavBar extends React.Component {
         this.showModal = this.showModal.bind(this);
         this.hideModal = this.hideModal.bind(this);
         this.homepage = this.homepage.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.demoSubmit = this.demoSubmit.bind(this);
     }
 
     showModal(type) {
@@ -31,7 +32,7 @@ class NavBar extends React.Component {
         this.props.history("/") 
     }
 
-    handleSubmit(e) {
+    demoSubmit(e) {
         e.preventDefault()
         this.props.login(this.props.demoUser).then(() => {
             this.props.history.push("/dashboard")
@@ -45,7 +46,7 @@ class NavBar extends React.Component {
             <div className="modal-nav">
                 <img className="nav-logo" onClick={this.homepage} src="https://www.brainscape.com/blog/wp-content/uploads/2018/07/Transparent-Background.png" />
                 <div className="nav-bar-buttons">
-                    <button className="demo-button" type="submit" onClick={this.handleSubmit}>Demo log in</button>
+                    <button className="demo-button" type="submit" onClick={this.demoSubmit}>Demo log in</button>
                     <div className="login-modal-div">
                         <LoginModal show={this.state.loginShow}
                             hideModal={this.hideModal}
--- frontend/components/user_page/dashboard.jsx
+++ frontend/components/user_page/dashboard.jsx
@@ -19,13 +19,19 @@ class Dashboard extends React.Component {
     render() {
         if (!this.props.user) return null;
         return (
-            <nav className="Dashboard-nav">
-                <p>Hello, {this.props.user.first_name} {this.props.user.last_name}</p>
-                <div className="dashboard-dropdown">
-                    <i class="fas fa-cog"></i>
-                    <button className="dashboard-logout" onClick={this.handleSubmit}>Log out</button>
+            <main className="Dashboard-main">
+                <section className="dashboard-user-section">
+                    <img className="dashboard-logo" src="https://www.brainscape.com/assets/bsc-share-icon.png" alt=""/>
+                    <p>Hello, {this.props.user.first_name} {this.props.user.last_name}</p>
+                    <div className="dashboard-dropdown">
+                        <i className="fas fa-cog"></i>
+                        <button className="dashboard-logout" onClick={this.handleSubmit}>Log out</button>
+                    </div>
+                </section>
+                <div className="current-class">
+                    Sample Class
                 </div>
-            </nav>   
+            </main>   
         )
     }
 }
