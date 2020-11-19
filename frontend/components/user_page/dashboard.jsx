44274b5484ea7e866b0d9d29043789dc89ba3086
Brandon Leong <bjtl1105@gmail.com>
2020-11-18 17:59:27 -0800

    styling the dashboard page to more resemble Brainscape

--- frontend/components/user_page/dashboard.jsx
+++ frontend/components/user_page/dashboard.jsx
@@ -23,24 +23,27 @@ class Dashboard extends React.Component {
         if (!this.props.user) return null;
         const userDecks = this.props.decks.map(deck => {
             if (deck.creator_id === this.props.user.id) {
-            return <li> <Link to={`/decks/${deck.id}`}>{deck.title}</Link> </li>
+            return <li className="deck-li"> <Link to={`/decks/${deck.id}`}>{deck.title}</Link> </li>
             }
         });
 
         return (
             <main className="Dashboard-main">
                 <section className="dashboard-user-section">
-                    <img className="dashboard-logo" src="https://www.brainscape.com/assets/bsc-share-icon.png" alt=""/>
-                    <p>Hello, {this.props.user.first_name} {this.props.user.last_name}</p>
-                    <div>
-                        Create a deck
-                        <Link to ="/dashboard/newDeck"> Create </Link>
-                    </div>
-                    <div className="dashboard-dropdown">
-                        <i className="fas fa-cog"></i>
-                        <button className="dashboard-logout" onClick={this.handleSubmit}>Log out</button>
-                    </div>
-                    <ul>
+                    <header className="dashboard-user-side">
+                        <img className="dashboard-logo" src="https://www.brainscape.com/assets/bsc-share-icon.png" alt=""/>
+                        <div className="greetings-and-info">
+                            <p>Hello, {this.props.user.first_name} {this.props.user.last_name}</p>
+                        </div> 
+                        <div>
+                            <Link to ="/dashboard/newDeck"> Create </Link>
+                        </div>
+                        <div className="dashboard-dropdown">
+                            <i className="fas fa-cog"></i>
+                            <button className="dashboard-logout" onClick={this.handleSubmit}>Log out</button>
+                        </div>
+                    </header>
+                    <ul className="deck-list">
                         {userDecks}
                     </ul>
                 </section>
