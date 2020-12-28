6b6db4e32be965ab90d9346c8fd573d5f49eb390
Brandon Leong <bjtl1105@gmail.com>
2020-12-28 13:13:13 -0800

    created a working deck review page where a user can go through a list of cards for a deck, navigate back to a previous question, and can go to the next card

--- frontend/components/deck_review/deck_review.jsx
+++ frontend/components/deck_review/deck_review.jsx
@@ -10,9 +10,12 @@ class DeckReview extends React.Component {
             reviewCards: [],
             currentCardIdx: 0,
             markedCards: [],
+            front: true,
         };
 
         this.nextCard = this.nextCard.bind(this);
+        this.previousCard = this.previousCard.bind(this);
+        this.revealAnswer = this.revealAnswer.bind(this);
     }
 
     componentDidMount() {
@@ -20,26 +23,60 @@ class DeckReview extends React.Component {
         this.props.requestCards();
     }
 
+    revealAnswer() {
+        this.setState({front: false});
+    }
+
     nextCard() {
-        debugger;
-        this.setState({currentCardIdx: this.state.currentCardIdx + 1});
+        // debugger;
+        this.setState({
+            currentCardIdx: this.state.currentCardIdx + 1,
+            front: true
+        });
+    }
+
+    previousCard() {
+        this.setState({ currentCardIdx: this.state.currentCardIdx - 1});
     }
 
     render() {
         if (!this.props.deck) return null;
         if (this.props.deckCards.length === 0) return null;
         // debugger;
-        return (
-            <div className="deck-review-page">  
-                <div className="deck-review-title">{this.props.deck.title}</div>
-                <div className ="deck-review-card">
+        const prevCard = this.state.currentCardIdx !== 0 ? (
+            <div className="prev-card-button" onClick={this.previousCard}>Go Back To the Previous Card</div>
+        ) : null
+        const frontOrBack = this.state.front === true ? (
+            <div className="card-review-front">
+                <div className="deck-review-card">
+                    <div className="current-card">
+                        {this.props.deckCards[this.state.currentCardIdx].front_side}
+                    </div>
+                </div>
+                <div className="card-review-button">
+                    {prevCard}
+                    <div onClick={this.revealAnswer} className="next-card-button">Reveal Answer</div>
+                </div>
+            </div>
+        ) : (
+            <div className="card-review-back">
+                <div className="deck-review-card">
                     <div className="current-card">
                         {this.props.deckCards[this.state.currentCardIdx].back_side}
                     </div>
                 </div>
                 <div className="card-review-button">
-                    <div onClick={this.nextCard} className="next-card">Go Hard</div>
+                    <div onClick={this.nextCard} className="next-card">Next Card</div>
                 </div>
+            </div>
+        )
+        return (
+            <div className="deck-review-page">  
+                <div className="deck-review-title">{this.props.deck.title}</div>
+                {frontOrBack}
+                {/* <div className="card-review-button">
+                    <div onClick={this.nextCard} className="next-card">Go Hard</div>
+                </div> */}
                 {/* <button className="reveal-button">Reveal Answer</button> */}
             </div>
         )
