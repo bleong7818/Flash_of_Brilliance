5069eba3f0ecccc6c45a5dc0c4c15fb091135ae4
Brandon Leong <bjtl1105@gmail.com>
2021-01-18 17:49:05 -0800

    edited the close button for the modal to account for future removeCardErrors functionality

--- frontend/components/cards/edit_card.jsx
+++ frontend/components/cards/edit_card.jsx
@@ -3,6 +3,12 @@ import React from 'react';
 class EditCard extends React.Component {
     constructor(props) {
         super(props);
+
+        this.handleClose = this.handleClose.bind(this);
+    }
+
+    handleClose() {
+        this.props.close();
     }
 
     render() {
@@ -10,7 +16,7 @@ class EditCard extends React.Component {
             <div className="edit-card-modal-bg">
                 <div className="edit-card-modal">
                     <div className="edit-deck-container">{this.props.card.front_side}
-                    <div className="edit-card-close" onClick={this.props.close}>×</div>
+                        <div className="edit-card-close" onClick={this.handleClose}>×</div>
                     </div>
                 </div>
             </div>
