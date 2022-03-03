a407fb7394761a4c2c7ded8329686e4b338cce24
Brandon Leong <bjtl1105@gmail.com>
2022-03-03 14:30:32 -0800

    pendo2

--- frontend/components/user_page/dashboard.jsx
+++ frontend/components/user_page/dashboard.jsx
@@ -47,9 +47,21 @@ class Dashboard extends React.Component {
         // const numberOfDecks = userDecks.length === 0 ? null : <p className="usermade-decks">{userDecks.length} decks created</p>
         return (
             <main className="Dashboard-main">
+                <script>
+                    (function(apiKey){
+                        (function (p, e, n, d, o) {
+                            var v, w, x, y, z; o = p[d] = p[d] || {}; o._q = o._q || [];
+                            v = ['initialize', 'identify', 'updateOptions', 'pageLoad', 'track']; for (w = 0, x = v.length; w < x; ++w)(function (m) {
+                                o[m] = o[m] || function () { o._q[m === v[0] ? 'unshift' : 'push']([m].concat([].slice.call(arguments, 0))); };
+                            })(v[w]);
+                            y = e.createElement(n); y.async = !0; y.src = 'https://cdn.pendo.io/agent/static/' + apiKey + '/pendo.js';
+                            z = e.getElementsByTagName(n)[0]; z.parentNode.insertBefore(y, z);
+                        })(window, document, 'script', 'pendo');
+})('3f1fc066-3ec9-460d-6a7b-c238a1fb5bf6');
+                </script>
                 <div className="dashboard-user-section">
                     <header className="dashboard-user-side">
-                        {/* <img className="dashboard-logo" src="https://www.brainscape.com/assets/bsc-share-icon.png" alt=""/> */}
+                        
                         <div className="greetings-and-info">
                             <p className="greeting"> Welcome, {this.props.user.first_name} {this.props.user.last_name}</p>
                             <div className="greeting-pt2">This is your profile page! Here you can look at decks that you've made and decks that you are studying.</div>
